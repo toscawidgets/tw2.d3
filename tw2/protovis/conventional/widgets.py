56f1735729dd3c1268d72656406ff863bb8b3aaa
Ralph Bean <ralph.bean@gmail.com>
2010-10-08 16:04:13 -0400

    added marks

--- tw2/protovis/conventional/widgets.py
+++ tw2/protovis/conventional/widgets.py
@@ -28,17 +28,35 @@ class AreaChart(twp.PVPanel):
                 w = %i,
                 h = %i,
                 x = pv.Scale.linear(data, function(d) d.x).range(0, w),
-                y = pv.Scale.linear(0, 5).range(0, h);
+                y = pv.Scale.linear(0, 4).range(0, h);
             """ % (self.py_data, self.p_width, self.p_height))
                           
         self.init().width(self.p_width).height(self.p_height) \
                 .bottom(self.p_bottom).top(self.p_top) \
                 .left(self.p_left).right(self.p_right)
 
+        self.add(pv.Rule) \
+                .data(js('x.ticks()')) \
+                .visible(js('function(d) { return d }')) \
+                .left(js('x')) \
+                .bottom(-5) \
+                .height(5) \
+                .add(pv.Label) \
+                .bottom(-5) \
+                .text(js('x.tickFormat'))
+
+        self.add(pv.Rule) \
+                .data(js('y.ticks(5)')) \
+                .bottom(js('y')) \
+                .strokeStyle(js('function(d) { return d ? "#eee" : "#000"}')) \
+                .anchor("left")\
+                .add(pv.Label).text(js('y.tickFormat'))
+
         self.add(pv.Area) \
-                .data(self.py_data) \
-                .bottom(1) \
-                .left(js('function(d) x(d.x)')) \
-                .height(twc.JSSymbol(src='function(d) y(d.y)')) \
-                .fillStyle(self.p_color) \
-                .anchor('top').add(pv.Line).lineWidth(3)
+                 .data(js('data')) \
+                 .bottom(1) \
+                 .left(js('function(d) x(d.x)')) \
+                 .height(twc.JSSymbol(src='function(d) y(d.y)')) \
+                 .fillStyle(self.p_color) \
+                 .anchor('top').add(pv.Line).lineWidth(3)
+
