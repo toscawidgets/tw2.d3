e408516c87559840568d72a7f065e953997fc3cd
Ralph Bean <ralph.bean@gmail.com>
2010-10-10 00:59:40 -0400

    updated demo description

--- tw2/protovis/conventional/__init__.py
+++ tw2/protovis/conventional/__init__.py
@@ -1,17 +1,8 @@
 """
-This package contains the basic core protovis resources to be
-used by the widgets packaged in the following:
+A bunch of cool widgets from protovis (under the 'conventional' heading in their
+examples page).
 
-    - tw2.protovis.conventional
-    - tw2.protovis.custom
-    - tw2.protovis.interaction
-    - tw2.protovis.hierarchies
-    - tw2.protovis.networks
-    - tw2.protovis.maps
-    - tw2.protovis.stats
-    - tw2.protovis.art
-
-Get this source from http://github.com/ralphbean/tw2.protovis.core
+Get this source from http://github.com/ralphbean/tw2.protovis.conventional
 
 Protovis itself can be found here:  http://vis.stanford.edu/protovis/
 """
