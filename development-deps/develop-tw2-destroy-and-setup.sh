97e5856aa222df7955debef5c980c500d0d71265
Ralph Bean <ralph.bean@gmail.com>
2011-03-15 16:08:51 -0400

    Using my repos instead of paj for devtools

--- development-deps/develop-tw2-destroy-and-setup.sh
+++ development-deps/develop-tw2-destroy-and-setup.sh
@@ -11,8 +11,8 @@ virtualenv $venv --no-site-packages
 source $venv/bin/activate
 pip install genshi
 pushd $devbase
-hg clone http://bitbucket.org/paj/tw2core || echo "tw2core exists."
-hg clone http://bitbucket.org/paj/tw2devtools || echo "tw2devtools exists."
+hg clone http://bitbucket.org/ralphbean/tw2core || echo "tw2core exists."
+hg clone http://bitbucket.org/ralphbean/tw2devtools || echo "tw2devtools exists."
 git clone http://github.com/ralphbean/tw2.protovis.core.git || echo "tw2.protovis.core exists."
 pushd tw2core;hg pull;python setup.py develop;popd
 pushd tw2devtools;hg pull;python setup.py develop;popd
