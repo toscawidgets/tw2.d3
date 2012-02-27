a7cb2e1c10dbe5a79d319d28959e12e15feebe1f
Ralph Bean <rbean@redhat.com>
2012-02-27 12:53:40 -0500

    Removed lingering reference to protovis.

--- tw2/d3/__init__.py
+++ tw2/d3/__init__.py
@@ -3,7 +3,7 @@ A bunch of cool widgets built on d3
 
 Get this source from http://github.com/ralphbean/tw2.d3
 
-Protovis itself can be found here:  http://mbostock.github.com/d3/
+d3 itself can be found here:  http://mbostock.github.com/d3/
 """
 
 from widgets import (
