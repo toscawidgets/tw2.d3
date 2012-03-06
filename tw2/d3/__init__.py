087e558e159ff56558d239ec534f6dab7304ce80
Ralph Bean <rbean@redhat.com>
2012-03-06 10:07:44 -0500

    Changed links to the new repo.

--- tw2/d3/__init__.py
+++ tw2/d3/__init__.py
@@ -1,7 +1,7 @@
 """
 A bunch of cool widgets built on d3
 
-Get this source from http://github.com/ralphbean/tw2.d3
+Get this source from http://github.com/toscawidgets/tw2.d3
 
 d3 itself can be found here:  http://mbostock.github.com/d3/
 """
